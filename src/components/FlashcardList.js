dfc9e2a177021e45cf3e6b01deaa15a1b81f08ef
cylee_ispark <tony.at.cp@gmail.com>
2023-06-08 23:38:35 +0900

    상단에 동물 종류에 따라 카테고리 추가

--- src/components/FlashcardList.js
+++ src/components/FlashcardList.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Flashcard from "./Flashcard";
 import '../styles/FlashcardList.css';
 
-function FlashcardList({ cards ,audio ,setAudio}) {
+function FlashcardList({ cards, audio, setAudio }) {
+
+  // button 활성화
+  let [isActive,setIsActive] = useState("");
+
+  // 선택된 카테고리 상태 저장
+  let [selectedCategory, setSelectedCategory] = useState("전체");
+
+  // // 카테 고리별로 동물 필터링
+  let filterCards = selectedCategory == "전체" 
+  ? cards
+  : cards.filter(card => card.category == selectedCategory)
+  
+
+  // // 카테고리 선택 핸들러
+  function handleCategorySelect(category) {
+    setSelectedCategory(category);
+  }
 
   let [flipCardID, setFlipCardID] = useState(null);
   let [showCount, setShowCount] = useState(15);
@@ -13,14 +30,23 @@ function FlashcardList({ cards ,audio ,setAudio}) {
 
   return (
     <>
+      {/* 카테고리 선택 영역 */}
+      <div className="category-selector">
+        <button className={selectedCategory == "전체" ? "on" : ""} onClick={() => { handleCategorySelect("전체") }}>전체</button>
+        <button className={selectedCategory == "애완,가축" ? "on" : ""} onClick={() => { handleCategorySelect("애완,가축") }}>애완,가축</button>
+        <button className={selectedCategory == "야생동물" ? "on" : ""} onClick={() => { handleCategorySelect("야생동물") }}>야생동물</button>
+        <button className={selectedCategory == "새" ? "on" : ""} onClick={() => { handleCategorySelect("새") }}>새</button>
+        <button className={selectedCategory == "곤충" ? "on" : ""} onClick={() => { handleCategorySelect("곤충") }}>곤충</button>
+      </div>
+
       <div className="flashcard-list">
         {
-          cards.slice(0, showCount).map((card) => <Flashcard card={card} flipCardID={flipCardID} setFlipCardID={setFlipCardID} audio={audio} setAudio={setAudio}/>)
+          filterCards.slice(0, showCount).map((card) => <Flashcard key={card.id} card={card} flipCardID={flipCardID} setFlipCardID={setFlipCardID} audio={audio} setAudio={setAudio} />)
         }
       </div>
 
       {
-        showCount >= cards.length
+        showCount >= filterCards.length
           ? null
           : <div className="btn-wrap">
             <button onClick={handleShowMore}>+ 더보기</button>
@@ -28,6 +54,6 @@ function FlashcardList({ cards ,audio ,setAudio}) {
       }
     </>
   )
-} 
- 
+}
+
 export default FlashcardList; 
--- src/data.js
+++ src/data.js
@@ -5,132 +5,154 @@ let data = [
     "name": "거위",
     "image": `${process.env.PUBLIC_URL + '/img/goose'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/goose'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 2,
     "name": "소",
     "image": `${process.env.PUBLIC_URL + '/img/cow'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/cow'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 3,
     "name": "양",
     "image": `${process.env.PUBLIC_URL + '/img/sheep'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/sheep'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 4,
     "name": "말",
     "image": `${process.env.PUBLIC_URL + '/img/horse'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/horse'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 5,
     "name": "닭",
     "image": `${process.env.PUBLIC_URL + '/img/chicken'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/chicken'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 6,
     "name": "고양이",
     "image": `${process.env.PUBLIC_URL + '/img/cat'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/cat'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 7,
     "name": "강아지",
     "image": `${process.env.PUBLIC_URL + '/img/dog'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/dog'}.mp3`,
+    "category" : "애완,가축"
   },
   {
     "id": 8,
     "name": "코끼리",
     "image": `${process.env.PUBLIC_URL + '/img/elephant'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/elephant'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 9,
     "name": "원숭이",
     "image": `${process.env.PUBLIC_URL + '/img/monkey'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/monkey'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 10,
     "name": "쥐",
     "image": `${process.env.PUBLIC_URL + '/img/rat'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/rat'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 11,
     "name": "사자",
     "image": `${process.env.PUBLIC_URL + '/img/lion'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/lion'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 12,
     "name": "뱀",
     "image": `${process.env.PUBLIC_URL + '/img/snake'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/snake'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 13,
     "name": "호랑이",
     "image": `${process.env.PUBLIC_URL + '/img/tiger'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/tiger'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 14,
     "name": "늑대",
     "image": `${process.env.PUBLIC_URL + '/img/wolf'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/wolf'}.mp3`,
+    "category" : "야생동물"
   },
   {
     "id": 15,
     "name": "삼색새",
     "image": `${process.env.PUBLIC_URL + '/img/three_color_bird'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/three_color_bird'}.mp3`,
+    "category" : "새"
   },
   {
     "id": 16,
     "name": "참새",
     "image": `${process.env.PUBLIC_URL + '/img/sparrow'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/sparrow'}.mp3`,
+    "category" : "새"
   },
   {
     "id": 17,
     "name": "뻐꾸기",
     "image": `${process.env.PUBLIC_URL + '/img/cuckoo'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/cuckoo'}.mp3`,
+    "category" : "새"
   },
   {
     "id": 18,
     "name": "까마귀",
     "image": `${process.env.PUBLIC_URL + '/img/crow'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/crow'}.mp3`,
+    "category" : "새"
   },
   {
     "id": 19,
     "name": "벌",
     "image": `${process.env.PUBLIC_URL + '/img/bee'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/bee'}.mp3`,
+    "category" : "곤충"
   },
   {
     "id": 20,
     "name": "파리",
     "image": `${process.env.PUBLIC_URL + '/img/fly'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/fly'}.mp3`,
+    "category" : "곤충"
   },
   {
     "id": 21,
     "name": "모기",
     "image": `${process.env.PUBLIC_URL + '/img/mosquito'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/mosquito'}.mp3`,
+    "category" : "곤충"
   },
   {
     "id": 22,
     "name": "귀뚜라미",
     "image": `${process.env.PUBLIC_URL + '/img/cricket'}.jpg`,
     "sound": `${process.env.PUBLIC_URL + '/sounds/cricket'}.mp3`,
+    "category" : "곤충"
   },
 ]
 
